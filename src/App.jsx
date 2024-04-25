cd2ff27070e52a228ee7d93f1333504677570129
Kunnikar <kunnikarboon@gmail.com>
2024-04-25 21:19:53 +0200

    Created react shopping app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Shopping App</h1>
+    </div>
+  );
+};
+
+export default App;
