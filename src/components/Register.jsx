1eeb593aaddf9199a4cad402c2e60a232847b925
Supanee <kunnikarboon@gmail.com>
2024-05-09 20:42:47 +0200

    modified Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,16 +2,16 @@ import "../index.css"
 import Card from "./Card";
 import useLocalStorage from "use-local-storage";
 import { Toggle } from "./Toggle";
-import useLocalStorage from "use-local-storage";
 import axios from "axios";
 import { useState } from "react";
 
-
+// set username, email and password to empty strings using useState hook.
  const Register = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+ 
+  // check if the email already exists in the database.
   const checkEmail = (users) => {
     const user = users.find((user) => user.email === email);
     if (user) {
@@ -20,7 +20,7 @@ import { useState } from "react";
     }
   };
 
-
+  // handle the submit event when the user clicks the submit button. 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const users = await axios.get("http://localhost:6001/users").then((res) => checkEmail(res.data, email ));
@@ -35,12 +35,12 @@ import { useState } from "react";
     setEmail("");
     setPassword("");  
   };
-
+    // set the preference to dark mode if the user's system is set to dark mode. 
     const preference = window.matchMedia('(prefers-color-scheme: dark)').matches;
         const [isDark, setIsDark] = useLocalStorage("isDark", preference);
 
   return (
-
+    // toggle button to switch between dark and light mode.
     <div className='App' data-theme={isDark ? "dark" : "light"} >
           <Toggle 
             isChecked={isDark}
